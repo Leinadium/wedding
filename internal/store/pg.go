b17f9bab81e2f64883412dd7234c17fb547d11c1
Leinadium <daniel.sch.guima@gmail.com>
2026-04-28 18:06:20 -0300

    WIP: moving and bootstraping svelte

--- internal/store/pg.go
+++ internal/store/pg.go
@@ -20,6 +20,9 @@ type PGStore struct {
 func NewPGStore(p Params) Service {
 	// dsn := "host=localhost user=gorm password=gorm dbname=gorm port=9920 sslmode=disable TimeZone=Asia/Shanghai"
 	dsn := p.DSN
+	if dsn == "" {
+		panic("dsn is required")
+	}
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
