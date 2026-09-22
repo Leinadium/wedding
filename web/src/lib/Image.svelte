4a22733842f64abd08975b516532c93fc2b5e2c8
Leinadium <daniel.sch.guima@gmail.com>
2026-09-21 21:56:49 -0300

    fix: removing filter from image

--- web/src/lib/Image.svelte
+++ web/src/lib/Image.svelte
@@ -8,38 +8,28 @@
     y,
     duration,
   }: {
-    src: string,
-    alt: string,
-    delay: number,
-    y: number,
-    duration: number,
+    src: string;
+    alt: string;
+    delay: number;
+    y: number;
+    duration: number;
   } = $props();
 
-  let loaded = $state(false)
+  let loaded = $state(false);
 </script>
 
-
-<img
-    src={src}
-    alt=""
-    onload={() => loaded = true}
-    style="display: none;"
-/>
+<img {src} alt="" onload={() => (loaded = true)} style="display: none;" />
 
 {#if loaded}
-	<img
-		src={src}
-		alt={alt}
-		transition:fly={{ delay: delay, y: y, duration: duration }}
-	/>
+  <img
+    {src}
+    {alt}
+    transition:fly={{ delay: delay, y: y, duration: duration }}
+  />
 {/if}
 
 <style>
   img {
-      width: 100%;
-      filter: drop-shadow(0px 20px 40px rgba(94, 58, 73, 0.3));
+    width: 100%;
   }
-
-
-
 </style>
